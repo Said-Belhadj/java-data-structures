3ff8e5b4652e7553bb87a09f9db460df0d7576b3
Said Belhadj <saidbelhadj0312@gmail.com>
2023-02-21 20:28:56 +0100

    LinkedList review

--- src/main/java/fr/sbelhadj/TheQueue.java
+++ src/main/java/fr/sbelhadj/TheQueue.java
@@ -1,10 +1,32 @@
 package fr.sbelhadj;
 
+import java.sql.PseudoColumnUsage;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class TheQueue {
     public static void main(String[] args) {
+
+        LinkedList<Person> linkedList = new LinkedList();
+        linkedList.add(new Person("Marie", 23));
+        linkedList.add(new Person("Ali", 34));
+
+        ListIterator<Person> listIterator = linkedList.listIterator();
+
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.next());
+        }
+
+        System.out.println("------Reverse------");
+
+        while (listIterator.hasPrevious()){
+            System.out.println(listIterator.previous());
+        }
+
+    }
+
+    private static void queues() {
         Queue<Person> supermarket = new LinkedList<>();
 
         supermarket.add(new Person("Marie", 23));
@@ -16,7 +38,6 @@ public class TheQueue {
         System.out.println("\nLook at the queue now : "+supermarket);
         supermarket.offer(new Person("Mariam", 27));
         System.out.println("\nAnother person came to the queue : "+supermarket);
-
     }
 
     static record Person(String name, int age){}
